759bc73f106a2abe5d93941904490089e920ceb8
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-10-03 15:25:15 +0500

    Splash screen added + Login Layout design updated

---
+++ app/src/main/java/com/example/twitterclone/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.twitterclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity_main);
+    }
+}
